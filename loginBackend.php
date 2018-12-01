354d9358ae998bb30545eabf881c45d4f85bd3be
Ahmed Hammad <hammad97official@gmail.com>
2018-12-01 19:26:48 +0200

    Documentation

--- loginBackend.php
+++ loginBackend.php
@@ -46,39 +46,4 @@
 
 
     }
-
-
-      //Lookup for username
-      /*$sql = "SELECT username, password, user_id FROM LoginForm WHERE username = '$username' ";
-      $result = mysqli_query($connection,$sql);
-      while ($row = mysqli_fetch_array($result)){
-        if($password == row["password"]){
-          echo "Welcome " . $username . " .. ";
-
-          //Get Id
-          $user_id = result["user_id"];
-
-          //Update last_login_date
-          $sql = "UPDATE LoginForm SET last_login_date = now() WHERE userId = '$user_id'";
-          if ($connection->query($sql) === TRUE) {
-              echo "Last login updated";
-              return;
-          } else {
-              echo "Error updating last record: " . $connection->error;
-              return;
-          }
-
-        }
-        else{
-          echo "Wrong password!";
-          return;
-        }
-
-        $connection->close();
-        echo "No username matched!";
-
-      }
-
-    }*/
-
 ?>
